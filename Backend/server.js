34675a75d186996041b1e5b28cac7b1589cdf75b
shani-tiwari <shanitiwari2021@gmail.com>
2025-11-18 16:23:49 +0530

    backend port added forr deployment

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
-
+const port = process.env.PORT || 3000;
 
 connectDB();
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('listening');
-})
+});
